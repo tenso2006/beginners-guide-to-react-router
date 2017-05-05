dd80b68c43189a96f5c2141d53e115a776158417
Tenzin Sonam <tenso2006@gmail.com>
2017-05-05 00:26:42 -0700

    Add: route to home and address

--- app/App.js
+++ app/App.js
@@ -1,6 +1,18 @@
-import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import React, { Component } from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
-const App = () => <h1>Hello World!</h1>
+// const App = () => <h1>Hello World!</h1>
+class App extends Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Home} />
+        <Route path='/address' component={Address} />
+      </Router>
+    );
+  }
+}
 
-export default App
+const Home = () => <h1>Welcome Home buddy</h1>
+const Address = () => <h2>Reach me at 123, GH st, SF, CA 90210 </h2>
+export default App;
