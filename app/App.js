875666a2a5b595b0faadddcfba0e5e575f3797d8
Tenzin Sonam <tenso2006@gmail.com>
2017-05-05 01:19:44 -0700

    Add: link to twitter and instagram

--- app/App.js
+++ app/App.js
@@ -6,13 +6,47 @@ class App extends Component {
   render() {
     return (
       <Router history={hashHistory}>
-        <Route path='/' component={Home} />
-        <Route path='/address' component={Address} />
+        <Route path='/' component={Container}>
+          <IndexRoute component={Home} />
+          <Route path='address' component={Address}>
+            <IndexRoute component={TwitterFeed}/>
+            <Route path='instagram' component={InstagramFeed} />
+          </Route>
+          <Route path='*' component={NotFound} />
+        </Route>
       </Router>
     );
   }
 }
 
 const Home = () => <h1>Welcome Home buddy</h1>
-const Address = () => <h2>Reach me at 123, GH st, SF, CA 90210 </h2>
+
+const Address = (props) => (
+  <div>
+    <br />
+    <Link to='/address'>Twitter Feed</Link>
+    <Link to='/address/instagram'>Instagram Feed </Link>
+    <h2>Reach me at 123, GH st, SF, CA 90210 </h2>
+    {props.children}
+  </div>
+)
+
+const NotFound = () => <h2>Sorry! This Page not is found!</h2>
+
+const Nav = () => (
+  <div>
+    <Link to='/'>Home</Link>
+    <Link to='/address'>My Address</Link>
+  </div>
+)
+
+const Container = (props) => (
+  <div>
+    <Nav />
+    {props.children}
+  </div>
+)
+
+const TwitterFeed = () => <h2>Twitter feed is On ;)</h2>
+const InstagramFeed = () => <h2>Instagram feed is so colorful :) :)</h2>
 export default App;
